4d5ab6db88143a94f79ab790e73428285bff52ae
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-11-12 05:41:33 +0300

    Update stock DB 2

--- stckmgt/models.py
+++ stckmgt/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import User
 class Stock(models.Model):
     item_no = models.CharField(max_length=50)
     color = models.CharField( max_length=50)
-    quantity = models.IntegerField(default=0 )
+    quantity = models.IntegerField("Quantity MTS",default=0 )
     opening_stck = models.IntegerField(default=0 )
     closing_stck = models.IntegerField(default=0 )
-    issue_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    issue_to = models.CharField(max_length=50)
+    issued_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    issued_to = models.CharField("Issued to",max_length=50)
     phone_number = models.CharField( max_length=50, blank = True)
     last_updated = models.DateTimeField( auto_now=False, auto_now_add=False)
 

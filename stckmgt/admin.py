ca2366cfeebeff739eb7d4ce95f52734d88f67fb
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-11-12 05:30:44 +0300

    Update stock DB

--- stckmgt/admin.py
+++ stckmgt/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Stock
 
 # Register your models here.
+
+@admin.register(Stock)
+class ViewAdmin(admin.ModelAdmin):
+    pass
+    
+
+
--- stckmgt/models.py
+++ stckmgt/models.py
@@ -3,10 +3,16 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Stock(models.Model):
-    item_no = models.CharField(("ITEM NO"), max_length=50)
+    item_no = models.CharField(max_length=50)
     color = models.CharField( max_length=50)
     quantity = models.IntegerField(default=0 )
+    opening_stck = models.IntegerField(default=0 )
+    closing_stck = models.IntegerField(default=0 )
     issue_by = models.ForeignKey(User, on_delete=models.CASCADE)
     issue_to = models.CharField(max_length=50)
-    phone_number = models.CharField( max_length=50)
+    phone_number = models.CharField( max_length=50, blank = True)
     last_updated = models.DateTimeField( auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return f"item: {self.item_no} | Color: {self.color}"
+        

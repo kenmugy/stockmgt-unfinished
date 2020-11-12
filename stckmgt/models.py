8e218a03819d62fd0a79f3bd4697af3dec42e409
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-11-12 22:48:46 +0300

    Added provision to make choices

--- stckmgt/models.py
+++ stckmgt/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+# from itemDb import ItemModel
 
 # Create your models here.
+
+# for c in Itemdb.objects.get('item_no'):
+#     item_choives
+
+item_choices = (
+    ("", ""),
+    ("V160YK", "V160YK"),
+    ("V001YK", "V001YK"),
+    ("V109YK", "V109YK"),
+    ("V121YK", "V121YK"),
+    ("V131YK", "V131YK"),
+    ("V133YK", "V133YK"),
+    ("V196YK", "V196YK"),
+)
+
 class Stock(models.Model):
-    item_no = models.CharField(max_length=50, blank = False, null = True)
+    item_no = models.CharField(max_length=50, choices = item_choices, blank = False, null = True)
     color = models.CharField( max_length=55, blank = False, null = True)
     quantity = models.IntegerField("Quantity MTS",default=0 )
     opening_stck = models.IntegerField(default=0 )

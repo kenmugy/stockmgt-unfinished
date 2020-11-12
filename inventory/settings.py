dc531aac77e7861ffc240b2e7660ad73749ae4c4
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-11-12 15:06:18 +0300

    Add crispy form styles

--- inventory/settings.py
+++ inventory/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'stckmgt'
+    'stckmgt',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRIPSY_TEMPLATE_PACK = 'bootstrap4'

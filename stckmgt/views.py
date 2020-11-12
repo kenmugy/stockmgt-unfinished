fc45c8219be4f69a7922c97dba0ac83161e636ec
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-11-12 05:53:09 +0300

    Create CreateView for creating Item

--- stckmgt/views.py
+++ stckmgt/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import Stock
 
 # Create your views here.
+
+class StockCreateView(CreateView):
+    model = Stock
+    template_name = "stckmgt/add_item.html"
